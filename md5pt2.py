74ae504ae1ada537c43b0a0a309bc4242766db27
arturoidelgado1998 <59622073+arturoidelgado1998@users.noreply.github.com>
2021-01-12 22:35:55 -0800

    md5 addeed the initialization function
    added the initialization function

--- md5pt2.py
+++ md5pt2.py
@@ -1,3 +1,21 @@
 import hashlib
 
-salt = 
+# paswword: $1$4fTgjp6q$JgdO/UQGRxKX2ZfmBIjt40
+def initilization():
+
+    password = input("password?")
+    salt = input("salt?")
+
+    hash=  password + "1$1" + salt
+
+    initilization1 = password
+
+    md5hashed = hashlib.md5(hash.encode('UTF-8'))
+    print(md5hashed.hexdigest())
+    lenpass = len(password)
+
+    while lenpass > 0:
+        lenpass = lenpass +
+
+
+initilization()
