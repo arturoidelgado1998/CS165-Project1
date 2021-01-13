c90cf89501ab614157c3ab2064912023e5023be7
Dominic-Renales <Renales.Dominic@gmail.com>
2021-01-12 16:28:12 -0800

    UPDATE: Added md5_Crypt and to64 function
    Some code translation taken from the CS165 Project 1 lab guidelines put into the main file.

--- MD5.cpp
+++ MD5.cpp
@@ -1,13 +1,30 @@
-
 #include <iostream>
-
 #include <string>
 
-using namespace std;
+const std::string base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
+std::string md5_Crypt();
+std::string to64(char, int);
 
+int main(int argC, char**argV)
+{
 
-int main(){
-    cout << "nive";
-    return 0;
+}
+
+std::string md5_Crypt()
+{
+	std::string h;
+	return to64((h[0] << 16) | (h[6] << 8) | (h[12]), 4) + 
+		   to64((h[1] << 16) | (h[7] << 8) | (h[13]), 4) + 
+		   to64((h[2] << 16) | (h[8] << 8) | (h[14]), 4) +
+		   to64((h[3] << 16) | (h[9] << 8) | (h[15]), 4) + 
+		   to64((h[4] << 16) | (h[10] << 8) | (h[5]), 4) + 
+		   to64(h[11], 2);
+}
+
+std::string to64(char v, int n)
+{
+	std::string ret = "";
+	for(unsigned int i = 0; i<n;i++)
+	return std::string();
 }
