f5a1175e998fb2ea73a83e00697590c69d0ae455
arturoidelgado1998 <59622073+arturoidelgado1998@users.noreply.github.com>
2021-01-14 22:09:27 -0800

    changed variable names and solved problem
    was not iterating at the same state but was set to another variable.

--- md5pt2.py
+++ md5pt2.py
@@ -1,13 +1,11 @@
 import hashlib
 
-
 # paswword: $1$4fTgjp6q$JgdO/UQGRxKX2ZfmBIjt40
-
 
 def to64(v,n):
     base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     ret = ""
-    for i in range(1,n+1):
+    for i in range(0,n):
         ret += base64[v&0x3f]
         v>>=6
     return ret
@@ -15,9 +13,11 @@ def to64(v,n):
 def initilization():
 
     password = "vnotpu"
-    pass2 =password.encode()
+    pass_bytes =bytes(password, 'utf-8')
+
     salt = "hfT7jp2q"
-    salt2 = salt.encode()
+    salt_bytes = salt.encode()
+    
     res = password + "$1$" + salt
     res2 = res.encode()
     hash= password+ salt + password
@@ -39,37 +39,37 @@ def initilization():
         else:
             res2 += password[0].encode()
         i >>= 1
-    print(res2)
+
     finalmd5 = hashlib.md5(res2)
-    finalmd52 = finalmd5.digest()
+    md5_res = finalmd5.digest()
 
-    hash = b'\xed\x7a\x53\x07\x58\x8e\x49\xed\x3a\x27\x77\xd9\x26\xd6\x2f\x96'
-    print(pass2)
-    for j in range(0,10):
+    for j in range(0,1000):
         tmp = b''
-        if (j %2) ==1:
-            tmp += pass2
-        else:
-            tmp += finalmd52
-        if (j % 3) != 0:
-            tmp += salt2
-            print(tmp)
-        if (j % 7) != 0:
-            tmp += pass2
-        if (j %2) == 1:
-            tmp+=finalmd52
-        else:
-            tmp += pass2
-    finalmd53 = hashlib.md5(tmp).digest()
+        tmp2 = ""
+        if (j %2) ==1: 
+            tmp += pass_bytes
+        else: 
+            tmp += md5_res 
+        if (j % 3) != 0: 
+            tmp += salt_bytes
+        if (j % 7) != 0: 
+            tmp += pass_bytes
+        if (j %2) == 1: 
+            tmp+=md5_res
+        else: 
+            tmp += pass_bytes
 
-    finalfinal = to64((finalmd53[0] << 16) | (finalmd53[6] << 8) | (finalmd53[12]), 4) +\
-    to64((finalmd53[1] << 16) | (finalmd53[7] << 8) | (finalmd53[13]), 4) +\
-    to64((finalmd53[2] << 16) | (finalmd53[8] << 8) | (finalmd53[14]), 4) +\
-    to64((finalmd53[3] << 16) | (finalmd53[9] << 8) | (finalmd53[15]), 4) +\
-    to64((finalmd53[4] << 16) | (finalmd53[10] << 8) | (finalmd53[5]), 4) +\
-    to64(finalmd53[11], 2)
+        md5_res = hashlib.md5(tmp).digest()
 
-    print(finalfinal)
+    final_hex = (to64((md5_res[0] << 16) | (md5_res[6] << 8) | (md5_res[12]), 4) +\
+    to64((md5_res[1] << 16) | (md5_res[7] << 8) | (md5_res[13]), 4) +\
+    to64((md5_res[2] << 16) | (md5_res[8] << 8) | (md5_res[14]), 4) +\
+    to64((md5_res[3] << 16) | (md5_res[9] << 8) | (md5_res[15]), 4) +\
+    to64((md5_res[4] << 16) | (md5_res[10] << 8) | (md5_res[5]), 4) +\
+    to64(md5_res[11], 2))
+
+    print(final_hex)
 
 
 initilization()
+
